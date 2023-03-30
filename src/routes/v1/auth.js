42ea0ce8b2e4fe2c237de7fcfcc48aad4d468a50
amilin <moh.amilin@moladin.com>
2023-03-30 14:28:11 +0700

    add validation

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const authControllers = require('../../modules/auth/controllers');
 
+const validate = require('../../validations/validate');
+const authValidation = require('../../validations/auth');
+
 const router = express.Router();
 
-router.post('/register', authControllers.register);
-router.post('/login', authControllers.login);
+router.post(
+    '/register',
+    validate(authValidation.register),
+    authControllers.register,
+);
+router.post('/login', validate(authValidation.login), authControllers.login);
 router.post('/logout', authControllers.logout);
 
 module.exports = router;
