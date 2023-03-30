3fce3731fcdf045ed5fec9504da8cb9fb927ed3e
amilin <moh.amilin@moladin.com>
2023-03-30 14:06:23 +0700

    middleware -> auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,51 @@
+const passport = require('passport');
+const httpStatus = require('http-status');
+
+const HandlerError = require('../utils/handleError');
+const { roleRights } = require('../config/roles');
+
+const verifyCallback =
+    (req, resolve, reject, requiredRights) => async (err, user, info) => {
+        if (err || info || !user) {
+            if (err) {
+                return reject(new HandlerError(httpStatus.UNAUTHORIZED, err));
+            }
+            return reject(
+                new HandlerError(
+                    httpStatus.UNAUTHORIZED,
+                    'Please authenticate',
+                ),
+            );
+        }
+        req.user = user;
+
+        if (requiredRights.length) {
+            const userRights = roleRights.get(user.role);
+            const hasRequiredRights = requiredRights.every((requiredRight) =>
+                userRights.includes(requiredRight),
+            );
+            if (!hasRequiredRights && req.params.userId !== user.id) {
+                return reject(
+                    new HandlerError(httpStatus.FORBIDDEN, 'Forbidden'),
+                );
+            }
+        }
+
+        resolve();
+    };
+
+const auth =
+    (...requiredRights) =>
+    async (req, res, next) => {
+        return new Promise((resolve, reject) => {
+            passport.authenticate(
+                'jwt',
+                { session: false },
+                verifyCallback(req, resolve, reject, requiredRights),
+            )(req, res, next);
+        })
+            .then(() => next())
+            .catch((err) => next(err));
+    };
+
+module.exports = auth;
